779b3c4e5ca7cdfe3519c32599fc6695144fe53f
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-06-11 17:25:58 -0500

    Josie's on a vacation far away...

---
+++ overly-excited.js
@@ -0,0 +1,13 @@
+// Create an array that contains the words in the sentence
+var sentence = [];
+
+// The addExcitement function should accept the array as the sole argument
+function addExcitement (theWordArray) {
+    /*
+      Write a `for` loop that iterates over the array argument and
+      outputs the words.
+     */
+}
+
+// Invoke the function and pass in the array
+addExcitement(sentence)
