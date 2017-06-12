8ed8973da30ce4802bea450315d2b5bcd2bc5864
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-06-11 22:22:06 -0500

    Close. Working on correcting spacing after added exclamation points.

--- overly-excited.js
+++ overly-excited.js
@@ -20,12 +20,13 @@
 // stackSong(sentence)
 
 
-
+// I USED THE ALPHA STACKER EXERCISE TO HELP ME WITH THIS EXERCISE, which is why you see directions 
+// about letters below...
 
 
 // Step 1:
 
-// Create an array that contains the letters of the alphabet, then send
+// Create an array that contains the words of your song, then send
 // it to the console so that it prints out.
 
 var sentence = ["Josie's", "on", "a", "vacation", "far", "away", "come", "around", "and", "talk", "it", "over" ];
@@ -37,7 +38,7 @@ console.log(sentence);
 // function stackSong (theSongArray) {
 
       // Write a `for` loop that iterates over the array argument and
-     //  outputs the letters so that they stack like this:
+     //  outputs the words so that they stack like this:
 
  //       a
  //       ab
@@ -55,9 +56,42 @@ var output = ' ';  /* Note: You have to declare var before filling it. You also 
 an empty string as a place holder. */
 
 for (var i = 0; i < sentence.length; i++ ) {
-    output += sentence[i]; /* Understand that += gives you where you're at currently and where you were. */
+    output += sentence[i] + " "; /* Understand that += gives you where you're at currently and where you were. */
     console.log(output);
   }
 }
 
-stackSong(); /* Here, you are calling the function you created. */
+// stackSong(); /* Here, you are calling the function you created. */
+
+
+
+
+// Step 3:
+
+// Now, add logic to stackSong that places a (!) after every third word.
+// This will require you to do some basic math in JavaScript, and use an if statement.
+// Your job is to read the following English statement and write
+// the equivalent in JavaSript code to make it work:
+
+// If the current value of the counter variable can be
+// evenly divided by 3 - using the JavaScript remainder operator - then add a single (!) to the output.
+
+
+function stackSong () {
+
+var output = ' ';
+
+for (var i = 0; i < sentence.length; i++ ) {
+    if (i % 3 === 0) {
+    output = output + " " + sentence[i] + '!';
+    console.log(output);
+    }
+
+else {
+    output = output + sentence[i];
+    console.log(output);
+   }
+ }
+}
+
+stackSong();
